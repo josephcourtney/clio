8390b198a9cd9bba694fb7c5d1de2f0ad1aace2f
josephcourtney <5942706+josephcourtney@users.noreply.github.com>
2025-04-22 16:41:52 -0400

    move python requirement to 3.12

--- src/clio/__init__.py
+++ src/clio/__init__.py
@@ -1,5 +1,5 @@
 # pyright: reportUnusedImport=false
 
-from .__version__ import __version__  # all:include-private
+from .__version__ import __version__  # awl:include-private
 
 __all__ = ["__version__"]
